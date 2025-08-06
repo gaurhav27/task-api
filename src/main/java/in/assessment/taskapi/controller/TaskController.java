0fe5bc617451a901e8a4c6943f987a68118af5fc
gaurhav27 <gaurhav.fido@gmail.com>
2025-08-07 00:50:27 +0530

    Added comments/descriptions for each controller endpoints

--- src/main/java/in/assessment/taskapi/controller/TaskController.java
+++ src/main/java/in/assessment/taskapi/controller/TaskController.java
@@ -17,22 +17,27 @@ import java.util.List;
 @RequestMapping("/tasks")
 public class TaskController {
 
+    //constructor injection for TaskService
+    @Autowired
     private final TaskService taskService;
 
     public TaskController(TaskService taskService) {
         this.taskService = taskService;
     }
 
+    // endpoint to create a new task
     @PostMapping("/createTask")
     public ResponseEntity<TaskResponse> createTask(@RequestBody TaskRequest taskRequest) {
        return ResponseEntity.ok(taskService.createTask(taskRequest));
     }
 
+    // endpoint to fetch all the tasks
     @GetMapping("/getAllTasks")
     public ResponseEntity<List<TaskResponse>> getAllTasks() {
         return ResponseEntity.ok(taskService.getAllTasks());
     }
 
+    // endpoint to get a specific task by its id
     @GetMapping("/getTaskById/{id}")
     public ResponseEntity<TaskResponse> getTaskById(@PathVariable Long id) {
         TaskResponse taskResponse = taskService.getTaskById(id);
@@ -42,6 +47,7 @@ public class TaskController {
         return ResponseEntity.ok(taskResponse);
     }
 
+    // endpoint to get a specific task by its status
     @GetMapping("/getTasksByStatus/{status}")
     public ResponseEntity<List<TaskResponse>> getTasksByStatus(@PathVariable String status) {
         return ResponseEntity.ok(taskService.getTasksByStatus(status));
